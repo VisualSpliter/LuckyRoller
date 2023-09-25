9b670304636bc6bbda34529f64063ff4f608e0e6
Issac Newtown <zstbmw@163.com>
2023-09-25 23:20:39 +0800

    Finish Title Check Function, Add New Examples and Docs

--- file_resolve.py
+++ file_resolve.py
@@ -12,26 +12,25 @@ from operator import itemgetter
 # Todo: Add "Choose Only One Class" Function
 #!Ditto
 # Todo: Add Title Check Function
-#*Done at 2021/9/24
+#*Done at 2021/9/25
 
-class Excel:
+class excel:
     def __init__(self) -> None:
         pass
     
     class resolve:
         def __init__(self) -> None:
             pass
-        
+       
         @classmethod
-        def file_resolve(self, excel_file_path = str, sheet_index = 0):
-
-            # If File Path / File is Blank, Raise an Error and Exit
-            if excel_file_path == None:
-                raise RuntimeError("File Can't be Blank, You Must Choose a File")
-            
+        def file_read(self, excel_file_path = str, sheet_index = 0):
 
             # Read The File in to Ram
-            student_sheet = pd.read_excel(excel_file_path, sheet_name = sheet_index)
+            student_sheet = pd.read_excel(excel_file_path, sheet_name = sheet_index )
+
+            #!Alert! Logic Improved and Isolated to Another Function Called "file_check"
+            # first_line = student_sheet.loc[0]
+            # print (first_line)
 
             return student_sheet
             
@@ -68,9 +67,36 @@ class Excel:
             class_list.sort()
 
             return class_list
+        @classmethod
+        def file_check(self, excel_file_path: str | None = None, sheet_index: int = 0):
+            # If File Path / File is Blank, Raise an Error and Exit
+            if excel_file_path == None:
+                raise RuntimeError("File Can't be Blank, You Must Choose a File")
+            
+            excel_file = pd.read_excel(excel_file_path,sheet_name=sheet_index,header=None)
+            file_header = list(excel_file.iloc[0])
+
+            for content in file_header:
+                if content == "班级":
+                    return True
+                else:
+                    raise RuntimeError("Excel File has a Header or Title")
+                
+                
 
     @staticmethod
-    def list_shuffle(student_list):
+    def list_shuffle(student_list : str):
+        '''
+        Params
+        --------
+        student_list : list
+        
+        Examples
+        --------
+        >>> student_list = Excel().list_shuffle(student_list)
+        >>> print(student_list)
+        '''
+
         random.shuffle(student_list)
         return student_list
     
@@ -85,11 +111,13 @@ class Excel:
 # student_list = excel_resolve(excel_file_path = excel_sheet)
 # print(student_list)
 
-stu_lst = Excel.resolve.student_list_resolve(excel_file_path="2023级计算机系本科新生录取名单.xlsx")
-class_lst = Excel.resolve.class_list(excel_file_path="2023级计算机系本科新生录取名单.xlsx")
+# stu_lst = Excel.resolve.student_list_resolve(excel_file_path="2023级计算机系本科新生录取名单_1.xlsx")
+# class_lst = Excel.resolve.class_list(excel_file_path="2023级计算机系本科新生录取名单_1.xlsx")
 
-print(stu_lst)
-print(class_lst)
+# print(stu_lst)
+# print(class_lst)
 
-stu_lst = Excel().list_shuffle(stu_lst)
-print(stu_lst)
+# stu_lst = Excel().list_shuffle(stu_lst)
+# print(stu_lst)
+
+excel.resolve.file_check("2023级计算机系本科新生录取名单_0.xlsx")
